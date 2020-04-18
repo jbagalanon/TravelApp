ba93250a936353fe08f337eef89839ab365e7af5
Joemar <jbagalanon@gmail.com>
2020-04-18 23:32:41 +0800

    VenueLogic, venue, edit trave;[age

--- TravelRecordApp/Model/Venue.cs
+++ TravelRecordApp/Model/Venue.cs
@@ -7,9 +7,9 @@ namespace TravelRecordApp.Model
 {
     public class Venue
     {
-        public static void GenerateURL(double latitude, double longitude)
+        public static string GenerateURL(double latitude, double longitude)
         {
-            string url = string.Format(Constants.VENUE_SEARCH, latitude, longitude, Constants.CLIENT_ID,Constants.CLIENT_SECRET, DateTime.Now.ToString("yyyyMMdd"));
+            return string.Format(Constants.VENUE_SEARCH, latitude, longitude, Constants.CLIENT_ID,Constants.CLIENT_SECRET, DateTime.Now.ToString("yyyyMMdd"));
         }
     }
 }
--- TravelRecordApp/NewTravelPage.xaml.cs
+++ TravelRecordApp/NewTravelPage.xaml.cs
@@ -4,7 +4,9 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Plugin.Geolocator;
 using SQLite;
+using TravelRecordApp.Logic;
 using TravelRecordApp.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -17,6 +19,16 @@ namespace TravelRecordApp
         public NewTravelPage()
         {
             InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var locator = CrossGeolocator.Current;
+            var position = await locator.GetPositionAsync();
+
+            var venues = VenueLogic.GetVenues(position.Latitude, position.Longitude);
         }
 
         private void ToolbarItem_Clicked(object sender, EventArgs e)
