819be4b87f6130d1b681fb820937c9e018999071
Joemar <jbagalanon@gmail.com>
2020-04-18 20:55:34 +0800

    Added API , venue and constants class

---
+++ TravelRecordApp/Helpers/Constants.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TravelRecordApp.Helpers
+{
+    class Constants
+    {
+        public const string VENUE_SEARCH = "https://api.foursquare.com/v2/venues/search?ll={0},{1}&client_id={2}&client_secret={3}&v={4}";
+
+        public const string CLIENT_ID = "AWJ5ZJFCGYYVO3RPMHNFZPHROWCY4APCMQC3PVDFQ4XMBGKS";
+
+        public const string CLIENT_SECRET = "H54KCBTJXIMJANZBWNMJW0J5HTNJIOVPCKK0SG5BISFCNB1L";
+    }
+}
