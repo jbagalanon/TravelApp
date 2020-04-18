29098d1e4e9e4be9d405915dea66b92bedf539e3
Joemar <jbagalanon@gmail.com>
2020-04-19 03:36:39 +0800

    Add map pins and stop listeninsynch

--- TravelRecordApp/MapPage.xaml.cs
+++ TravelRecordApp/MapPage.xaml.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Plugin.Geolocator;
+using SQLite;
+using TravelRecordApp.Model;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
@@ -19,24 +21,73 @@ namespace TravelRecordApp
             InitializeComponent();
         }
 
-  
+
         // this is the old code - protected override void OnAppearing()
         protected async override void OnAppearing()
         {
             base.OnAppearing();
-            
+
             //this is the original file
-             var locator = CrossGeolocator.Current;
-             locator.PositionChanged += Locator_PositionChanged;
-             await locator.StartListeningAsync(new TimeSpan(0), 100);
+            var locator = CrossGeolocator.Current;
+            locator.PositionChanged += Locator_PositionChanged;
+            await locator.StartListeningAsync(new TimeSpan(0), 100);
 
-             var position = await locator.GetPositionAsync();
+            var position = await locator.GetPositionAsync();
 
-             var center = new Xamarin.Forms.Maps.Position(position.Latitude, position.Longitude);
-             var span = new Xamarin.Forms.Maps.MapSpan(center,2,2);
+            var center = new Xamarin.Forms.Maps.Position(position.Latitude, position.Longitude);
+            var span = new Xamarin.Forms.Maps.MapSpan(center, 2, 2);
             locationsMap.MoveToRegion(span);
 
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                // these are generics method   conn.CreateTable<Post>(),conn.Table<Post>()
+
+                conn.CreateTable<Post>();
+                var posts = conn.Table<Post>().ToList();
+
+                DisplayInMap(posts);
+            }
+
         }
+
+        protected override async void OnDisappearing()
+        {
+
+            var locator = CrossGeolocator.Current;
+            base.OnDisappearing();
+            locator.PositionChanged -= Locator_PositionChanged;
+
+
+            locator.StopListeningAsync();
+        }
+
+        private void DisplayInMap(List<Post> posts)
+        {
+            foreach (var post in posts)
+            {
+                try
+                {
+                    var position = new Xamarin.Forms.Maps.Position(post.Latitude, post.Longitude);
+                    var pin = new Xamarin.Forms.Maps.Pin()
+                    {
+                        Type = Xamarin.Forms.Maps.PinType.SavedPin,
+                        Position = position,
+                        Label = post.VenueName,
+                        Address = post.Address
+                    };
+                    locationsMap.Pins.Add(pin);
+                }
+                catch (NullReferenceException nre)
+                {
+
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
 
         private void Locator_PositionChanged(object sender, Plugin.Geolocator.Abstractions.PositionEventArgs e)
         {
@@ -45,7 +96,7 @@ namespace TravelRecordApp
             locationsMap.MoveToRegion(span);
 
         }
-      
+
     }
 }
 
