2214533769eb333e5090739b3d15806c10d6e68a
Joemar <jbagalanon@gmail.com>
2020-04-19 02:33:25 +0800

    Modify post model and took value in venue model

--- TravelRecordApp/Model/Post.cs
+++ TravelRecordApp/Model/Post.cs
@@ -16,5 +16,18 @@ namespace TravelRecordApp.Model
         public string Experience { get; set; }
 
 
+        public string VenueName { get; set; }
+
+        public string CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+        public string Address { get; set; }
+
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public int Distance { get; set; }
+
+
     }
 }
